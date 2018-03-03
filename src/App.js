60d74920e6a44aef1afc4d2457c4e6fb31ae1573
Alex <saniaivanov94@gmail.com>
2018-03-03 16:30:26 +0100

    Front-end in progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
 
+// Components
+import JamonFinder from "./components/JamonFinder";
+import JamonCard from "./components/JamonCard";
+
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
@@ -67,6 +71,10 @@ class App extends Component {
     })
   }
 
+  handleSearch(value) {
+    console.log(value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -77,12 +85,10 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <h1>Jamon forever!</h1>
+              <h2>Search</h2>
+              <JamonFinder handleSearch={this.handleSearch.bind(this)} />
+              <JamonCard name="Pata Negra"/>
             </div>
           </div>
         </main>
