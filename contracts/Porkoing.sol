4f1e548e48739dc916a4683548df86c1811526a3
Jose <jomoespe@gmail.com>
2018-03-03 17:06:55 +0100

    Update Poroing contract

--- contracts/Porkoing.sol
+++ contracts/Porkoing.sol
@@ -8,26 +8,38 @@ contract Porkoing {
     mapping(uint => Pork)   porks;
 
     struct Farmer {
+        uint id;
         string name;
     }
 
     struct Vet {
         uint id;
+        string name;
     }
 
     struct Seller {
         uint id;
+        string name;
     }
 
     struct Pork {
-        uint id;
-        uint farmerId;
+        uint   id;
+        Farmer farmer;
         string birthdate; 
         string breed;
+        bool   vaccinated;
+        Vet    vet;
+        Seller seller;
     }
 
     function Porkoing() public {
         owner = msg.sender;
+        // TODO initializa fields
+        farmers[1] = Farmer( {id: 1, name: "Pablo"} );
+        farmers[2] = Farmer( {id: 2, name: "Jose"} );
+        vets[1] = Vet( {id: 3, name: "Lucia"} );
+        sellers[1] = Seller( {id: 4, name: "Victor"} );
+        sellers[1] = Seller( {id: 4, name: "Alex"} );
     }
 
     function create(uint farmerId, uint porkId, string birthdate, string breed) public {
@@ -37,19 +49,26 @@ contract Porkoing {
             //farmers[farmerId] &&
             //porks[porkId]
         );
-        porks[porkId] = Pork({id:porkId, farmerId: farmerId, birthdate: birthdate, breed: breed});
-    }
-/*
-    function vacine(porkId, vetId) {
-        // el veterinadio en la lista
+        porks[porkId] = Pork({id:porkId,
+                             farmer: farmers[farmerId],
+                             birthdate: birthdate,
+                             breed: breed,
+                             vaccinated: false,
+                             vet: Vet({id: 0, name: ""}),
+                             seller: Seller({id: 0, name: ""})
+                            });
     }
 
-    function sell(porkId, sellerId) {
-        // tiene que estar vacunado
+    function vacine(uint porkId, uint vetId) public {
+        porks[porkId].vaccinated = true;
+        porks[porkId].vet = vets[vetId];
     }
-    
-    function findPork(porkId) {
 
+    function sell(uint porkId, uint sellerId) public {
+        porks[porkId].seller = sellers[sellerId];
     }
-    */ 
+
+    function findPork(uint porkId) public view returns (Pork) {
+        return porks[porkId];
+    }
 }
